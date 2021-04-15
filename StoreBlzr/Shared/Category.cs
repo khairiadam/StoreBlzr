7f993a0ba0bcc038f93f731c5c97cad170f6e1ec
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-04-15 14:51:30 +0000

    Add Image To Category

--- StoreBlzr/Shared/Category.cs
+++ StoreBlzr/Shared/Category.cs
@@ -8,6 +8,7 @@ namespace Shared
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Images Image { get; set; }
         public List<Product> Products { get; set; }
     }
 }
