bfc6fe4e787d24e2d25d77b2e8aa53b117ad65b4
therainmaiker <57842299+therainmaiker@users.noreply.github.com>
2021-04-16 11:50:04 +0000

    Create Methods in Interface then fixing Implimentation

--- StoreBlzr/Server/Services/Products/ProductService.cs
+++ StoreBlzr/Server/Services/Products/ProductService.cs
@@ -1,7 +1,69 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Shared;
+
 namespace Server.Services.Products
 {
     public class ProductService : IProductService
     {
+        private readonly StoreDbContext _context;
+
+        public ProductService(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+
+
+        }
+
+
+
+
+        public async Task<Product> Product(string Id)
+        {
+            var getProduct = await _context.Products.FindAsync(Id);
+
+
+            return getProduct;
+
+
+        }
+
+        public async Task<Product> CreateProduct(Product model)
+        {
+            await _context.Products.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model;
+
+
+        }
+
+        public async Task<string> DeleteProduct(string Id)
+        {
+            var DeleteProdcut = await _context.Products.FindAsync(Id);
+
+            _context.Products.Remove(DeleteProdcut);
+            await _context.SaveChangesAsync();
+
+            return ("Product has been Deleted");
+        }
+
+        public async Task<string> UpdateProduct(string Id, Product model)
+        {
+            var GetProduct = await _context.Products.FindAsync(Id);
+
+            _context.Products.Update(GetProduct);
+
+            return ("Product has been updated");
+
+        }
 
     }
 }
