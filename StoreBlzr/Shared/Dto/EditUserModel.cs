007805173cdd3ac3465375eedf76fdae4dc18528
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-04-16 10:31:17 +0000

    Allow null in Edit user model

--- StoreBlzr/Shared/Dto/EditUserModel.cs
+++ StoreBlzr/Shared/Dto/EditUserModel.cs
@@ -1,27 +1,36 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Shared.Dto
 {
     public class EditUserModel
     {
-        public string? FirstName { get; set; }
+        [AllowNull]
+        public string FirstName { get; set; }
 
-        public string? LastName { get; set; }
+        [AllowNull]
+        public string LastName { get; set; }
 
-        public string? UserName { get; set; }
+        [AllowNull]
+        public string UserName { get; set; }
 
-        public string? Email { get; set; }
+        [AllowNull]
+        public string Email { get; set; }
 
-        public string? PhoneNumber { get; set; }
+        [AllowNull]
+        public string PhoneNumber { get; set; }
 
         [DataType(DataType.Password)]
-        public string? Password { get; set; }
+        [AllowNull]
+        public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        public string? NewPassword { get; set; }
+        [AllowNull]
+        public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
         [Compare("NewPassword")]
-        public string? ConfirmNewPassword { get; set; }
+        [AllowNull]
+        public string ConfirmNewPassword { get; set; }
     }
 }
