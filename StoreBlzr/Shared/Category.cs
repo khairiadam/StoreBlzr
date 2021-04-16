c388ccc0f3bd6601224c8c77181f07b1fc19874e
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-04-16 10:39:22 +0000

    Fix JsonIgnore

--- StoreBlzr/Shared/Category.cs
+++ StoreBlzr/Shared/Category.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Shared
 {
@@ -13,6 +14,7 @@ namespace Shared
         public string Name { get; set; }
         public string Description { get; set; }
         public Images Image { get; set; }
+        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
