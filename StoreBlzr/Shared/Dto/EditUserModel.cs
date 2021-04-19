eefec99dac7e9cd94cb6112c51ce82e769055ed8
adamkhairi <57996941+adamkhairi@users.noreply.github.com>
2021-04-19 10:17:10 +0000

    edit userService

--- StoreBlzr/Shared/Dto/EditUserModel.cs
+++ StoreBlzr/Shared/Dto/EditUserModel.cs
@@ -1,10 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace StoreBlzr.Shared.Dto
 {
     public class EditUserModel
     {
+        public string Id { get; set; }
+
         [AllowNull]
         public string FirstName { get; set; }
 
@@ -20,17 +23,21 @@ namespace StoreBlzr.Shared.Dto
         [AllowNull]
         public string PhoneNumber { get; set; }
 
+        [JsonIgnore]
         [DataType(DataType.Password)]
-        [AllowNull]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
+        [JsonIgnore]
         [DataType(DataType.Password)]
         [AllowNull]
         public string NewPassword { get; set; }
 
+        [JsonIgnore]
         [DataType(DataType.Password)]
-        [Compare("NewPassword")]
+        [Compare("NewPassword",ErrorMessage = "Passwords are not identical")]
         [AllowNull]
         public string ConfirmNewPassword { get; set; }
+        public string Message { get; set; }
     }
 }
