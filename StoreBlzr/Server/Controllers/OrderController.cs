dbb94805fb9f6d78b82040a8f31db90a61ac7292
Salma Belassiri <salmabelassiri@gmail.com>
2021-04-20 15:03:40 +0000

    Update OrderController.cs

--- StoreBlzr/Server/Controllers/OrderController.cs
+++ StoreBlzr/Server/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Server.Services;
+using StoreBlzr.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,36 +14,71 @@ namespace StoreBlzr.Server.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly ITypeCrud<Order> _order;
+
+        public OrderController(ITypeCrud<Order> order)
+        {
+            _order = order;
+        }
+
+
         // GET: api/<OrderController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> Orders()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(await _order.GetAll());
         }
 
         // GET api/<OrderController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("Detail")]
+        public async Task<IActionResult> GetOrder(string id)
         {
-            return "value";
+            if (string.IsNullOrEmpty(id)) return NotFound("Invalid Order Id !");
+
+            var clt = await _order.Get(id);
+
+            if(clt is null) return NotFound("Order was not Found !");
+
+            return Ok(clt);
         }
 
         // POST api/<OrderController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddOrders(Order order)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("There is an error");
+            }
+
+            await _order.Post(order);
+            return Ok(order);
+
         }
 
-        // PUT api/<OrderController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
 
         // DELETE api/<OrderController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteOrder(string id)
         {
+            if (string.IsNullOrEmpty(id)) return BadRequest();
+
+           await _order.Delete(id);
+
+            return Ok();
+
         }
+
+
+        // PUT api/<OrderController>/5
+        [HttpPut("Edit")]
+        public async Task<IActionResult> Update(string id, Order order)
+        {
+
+            await _order.Put(order);
+
+            return Ok(order);
+        }
+
     }
 }
